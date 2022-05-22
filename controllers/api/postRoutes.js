f7a729fe3122b2754beb25c616a4c48a1ac0e50e
Eric Hurst <ericahurst77@gmail.com>
2022-05-22 15:59:25 -0400

    add post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -11,3 +11,28 @@ const getDashboardPost = async (req, res) => {
   const postParsing = formatParsing(posts);
   res.render('dashboard', { title: 'Dashboard', posts: postParsing });
 };
+
+const getHomePost = async (req, res) => {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        as: 'User',
+      },
+    });
+    const postParsing = formatParsing(posts);
+    res.render('home', { title: 'The Tech Blog', posts: postParsing });
+  };
+  
+  const getAddPostHandlerbar = (req, res) => {
+    res.render('addpost', { title: 'Dashboard' });
+  };
+  
+  const addPost = async (req, res) => {
+    const post = Post.build({
+      title: req.body.title,
+      content: req.body.content,
+      userId: req.userId,
+    });
+    await post.save();
+    res.redirect('/');
+  };
