f6c1ae814320f63db910fb37455df843a0e731e0
Eric Hurst <ericahurst77@gmail.com>
2022-05-21 20:56:36 -0400

    add user.js

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+console.log(sequelize);
+const User = sequelize.define(
+  'User',
+  {
+    // Model attributes are defined here
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// A BelongsTo
+
+// (async function(){
+//   await User.sync()
+//   // await User.drop();
+// })()
+
+module.exports = User;
